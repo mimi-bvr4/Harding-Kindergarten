e91b4b893eb4cdf7e41e5c177b32ea578fc8eb28
mimi-bvr4 <mimi@infinityhospitality.net>
2026-01-16 20:50:21 -0600

    Update dashboard-data.js

--- dashboard-data.js
+++ dashboard-data.js
@@ -1,26 +1,27 @@
 // This file stores the dashboard data
-// Updated by admin-dashboard.html
+// Last updated: 1/16/2026, 8:46:47 PM
 
 const dashboardData = {
-    lastUpdated: "2026-01-16T20:00:00",
-    weeklyEmail: {
-        week: "Week of January 13-17, 2026",
-        compassUrl: "",
-        emailUrl: "",
-        kindergartenExcerpt: "",
-        highlights: [
-            "Thank you to all parents who attended the winter concert!",
-            "Reminder: Book fair is next week, Monday-Friday in the library",
-            "Lost and found items will be donated on Jan 31st - please check!",
-            "New lunch menu available on the school website",
-            "Registration for spring sports opens February 1st"
+    "lastUpdated": "2026-01-17T02:46:45.513Z",
+    "weeklyEmail": {
+        "week": "",
+        "compassUrl": "https://hardingacademy.myschoolapp.com/podium/push/default.aspx?i=698278&s=287&snd=a33ab57b-19db-400e-8915-27a1ea63bd15",
+        "emailUrl": "",
+        "kindergartenExcerpt": "",
+        "highlights": [
+            "Kindergarten Spirit Night!  Please join your fellow kindergarten friends on Friday, January 23rd to cheer on the Harding girls basketball team!  All Kindergarten families are encouraged to show their Harding spirit at this fun sporting event!  ",
+            "Please continue to save boxes (of all sizes) for our Kindergarten adventure called Roxaboxen which begins January 26th.  We will ask you to bring boxes in on Monday, January 26th.  "
         ],
-        importantDates: [
-            "Jan 20 - MLK Day (No School)",
-            "Jan 24 - Parent-Teacher Conferences 3-7 PM",
-            "Feb 14 - Valentine's Day Party"
+        "importantDates": [
+            "January 19- Dr. Martin Luther King, Jr. Day (school holiday) ",
+            "January 23- Kindergarten Spirit Basketball Game at 5:30pm ",
+            "January 29- Rox-a-boxen Drive in Movie (4:00-6:00pm in the theater) Please sign up by purchasing the auction item!",
+            "February 12- Valentine’s Day Party (information sent home on pink sheet in student folders) ",
+            "February 13 and February 16- School holidays (President's Weekend) "
         ]
     },
+    "schoolEvents": []
+};
     schoolEvents: [
         { date: '2026-01-20', title: 'Martin Luther King Jr. Day', type: 'holiday', location: 'No School' },
         { date: '2026-01-24', title: 'Parent-Teacher Conferences', type: 'meeting', location: 'Main Building' },
