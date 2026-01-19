80de776e221d869f51125dbd53e6623c3fb115f4
mimi-bvr4 <mimi@infinityhospitality.net>
2026-01-19 10:24:16 -0600

    Update dashboard-data.js

--- dashboard-data.js
+++ dashboard-data.js
@@ -1,13 +1,13 @@
 // This file stores the dashboard data
-// Last updated: 1/16/2026, 9:17:05 PM
+// Last updated: 1/19/2026, 10:20:05 AM
 
 const dashboardData = {
-    "lastUpdated": "2026-01-17T03:17:03.126Z",
+    "lastUpdated": "2026-01-19T16:20:05.620Z",
     "weeklyEmail": {
         "week": "",
-        "compassUrl": "https://hardingacademy.myschoolapp.com/podium/push/default.aspx?i=698278&s=287&snd=a33ab57b-19db-400e-8915-27a1ea63bd15",
+        "compassUrl": "https://hardingacademy.myschoolapp.com/podium/push/default.aspx?i=699483&s=287&snd=9c0616b2-fa05-4377-8b9d-4095e3f5f3d4",
         "emailUrl": "",
-        "kindergartenExcerpt": "",
+        "kindergartenExcerpt": "Please send in pants/leggings labeled with your child’s name in a baggie. If your student has had to borrow clothing (pants, skirts, leggings, etc.) from school, please be sure to return these items washed the next day. Thank you so much for your help!!! ",
         "highlights": [
             "Kindergarten Spirit Night!  Please join your fellow kindergarten friends on Friday, January 23rd to cheer on the Harding girls basketball team!  All Kindergarten families are encouraged to show their Harding spirit at this fun sporting event!  ",
             "Please continue to save boxes (of all sizes) for our Kindergarten adventure called Roxaboxen which begins January 26th.  We will ask you to bring boxes in on Monday, January 26th.  "
@@ -19,6 +19,5 @@ const dashboardData = {
             "February 12- Valentine’s Day Party (information sent home on pink sheet in student folders) ",
             "February 13 and February 16- School holidays (President's Weekend) "
         ]
-    },
-    "schoolEvents": []
+    }
 };
