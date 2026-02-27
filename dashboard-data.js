2e9f62cc6ef47afbb056e60dc52271a9320e3473
mimi-bvr4 <mimi@infinityhospitality.net>
2026-02-27 16:50:50 -0600

    Update dashboard-data.js

--- dashboard-data.js
+++ dashboard-data.js
@@ -1,23 +1,19 @@
 // This file stores the dashboard data
-// Last updated: 1/19/2026, 10:20:05 AM
+// Last updated: 2/27/2026, 4:49:15 PM
 
 const dashboardData = {
-    "lastUpdated": "2026-01-19T16:20:05.620Z",
+    "lastUpdated": "2026-02-27T22:49:15.843Z",
     "weeklyEmail": {
         "week": "",
-        "compassUrl": "https://hardingacademy.myschoolapp.com/podium/push/default.aspx?i=699483&s=287&snd=9c0616b2-fa05-4377-8b9d-4095e3f5f3d4",
-        "emailUrl": "",
-        "kindergartenExcerpt": "Please send in pants/leggings labeled with your child’s name in a baggie. If your student has had to borrow clothing (pants, skirts, leggings, etc.) from school, please be sure to return these items washed the next day. Thank you so much for your help!!! ",
-        "highlights": [
-            "Kindergarten Spirit Night!  Please join your fellow kindergarten friends on Friday, January 23rd to cheer on the Harding girls basketball team!  All Kindergarten families are encouraged to show their Harding spirit at this fun sporting event!  ",
-            "Please continue to save boxes (of all sizes) for our Kindergarten adventure called Roxaboxen which begins January 26th.  We will ask you to bring boxes in on Monday, January 26th.  "
-        ],
+        "compassUrl": "https://hardingacademy.myschoolapp.com/podium/push/default.aspx?i=704829&s=287&snd=dd4471d7-dab9-4f5c-97fa-f38406833b95",
+        "emailUrl": "https://hardingacademy.myschoolapp.com/podium/push/default.aspx?s=287&i=706278&u=0",
+        "kindergartenExcerpt": "We were all thrilled to be back in full swing of school, teaching and learning together!  We have cherished the routine and predictable schedule we had at the end of last week and beginning of this week.  \n\nWe began our narrative writing unit this week by making a Heart Map and choosing one thing to write about.  Students decided on a beginning, middle and end sentence to get them started.  We will add more “middle” sentence details next week and hopefully have a published book before Spring Break. \n\nIn math, students are labeling patterns and also creating patterns using a label.  These patterns can have up to four variables!  It is a lot of fun seeing how creative they can be when they have the opportunity to challenge themselves! We are also solidifying our understanding of subtraction.  Students are distinguishing between an addition and subtraction sign and solving the number sentence accordingly.  We are working on using the “cross out method” to solve subtraction sentences.  We will continue to learn additional strategies to solve both addition and subtraction problems throughout the remainder of the year.",
+        "highlights": [],
         "importantDates": [
-            "January 19- Dr. Martin Luther King, Jr. Day (school holiday) ",
-            "January 23- Kindergarten Spirit Basketball Game at 5:30pm ",
-            "January 29- Rox-a-boxen Drive in Movie (4:00-6:00pm in the theater) Please sign up by purchasing the auction item!",
-            "February 12- Valentine’s Day Party (information sent home on pink sheet in student folders) ",
-            "February 13 and February 16- School holidays (President's Weekend) "
+            "3/12: JUMP AND HOOP IT UP - Parents may attend and cheer on their child(ren) ",
+            "during their PE time = 8:55-9:25am (more info from the school will be ",
+            "communicated soon!) ",
+            "3/13 - 3/22:  Spring Break! "
         ]
     }
 };
